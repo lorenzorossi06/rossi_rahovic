dd13aaab4655d6b4aa333abb0109a576978b02a4
lore-red <rossi.lorenzo.06@itisdalmine.edu.it>
2023-05-21 11:11:11 +0200

    aggiunto id ai pulsanti

---
+++ SITO/script.js
@@ -0,0 +1,21 @@
+function assignButtonIds() {
+    const lis = document.querySelectorAll('.nav.one ul li');
+    for(let i = 0; i < lis.length; i++) {
+      const li = lis[i];
+      const id = `button-${i + 1}`;
+      li.setAttribute('id', id);
+    }
+  }
+  
+  function addNavListener() {
+    const lis = document.querySelectorAll('.nav.one ul li');
+    for(let i = 0; i < lis.length; i++) {
+      const li = lis[i];
+      li.addEventListener('click', function(event) {
+        console.log(event.target.id);
+      });
+    }
+  }
+  
+  assignButtonIds();
+  addNavListener();
