9d4f31e19401e8252378707c09b689eb36701255
lore-red <rossi.lorenzo.06@itisdalmine.edu.it>
2023-05-28 18:57:53 +0200

    format gruppi.json

--- SITO/script.js
+++ SITO/script.js
@@ -1,83 +1,72 @@
-const secondDiv = document.querySelector('.nav.two > ul')
+const secondDiv = document.querySelector(".nav.two > ul");
 let data;
 let button;
 let Item = [];
-let selezionato = { "R" : null,
-                  "oggetto": null,
-                  "nome": null}
+let selezionato = { R: null, oggetto: null, nome: null };
 
 function assignButtonIds(tag) {
   const lis = document.querySelectorAll(tag);
   for (let i = 0; i < lis.length; i++) {
     const li = lis[i];
     const id = i;
-    li.setAttribute('id', id);
+    li.setAttribute("id", id);
   }
 }
 
 function addActive(lis, clicked) {
-  lis.forEach(li=>{
-    li.classList.remove("active")
-  })
-  clicked.classList.add("active")
+  lis.forEach((li) => {
+    li.classList.remove("active");
+  });
+  clicked.classList.add("active");
 }
 
 function addNavListener(tag, funzione) {
   const lis = document.querySelectorAll(tag);
   for (let i = 0; i < lis.length; i++) {
     const li = lis[i];
-    li.addEventListener('click', function(event) {
-      addActive(lis,li)      
+    li.addEventListener("click", function (event) {
+      addActive(lis, li);
       button = event.target.id;
       let content = event.target.textContent.trim();
 
-      if (funzione == 1){
-        selezionato["oggetto"] = content
-        console.log(selezionato)
+      if (funzione == 1) {
+        selezionato["oggetto"] = content;
+        console.log(selezionato);
+      } else {
+        selezionato = { R: null, oggetto: null, nome: null };
+        selezionato["R"] = content;
+        displayData();
       }
-
-      else{
-        selezionato = { "R" : null,
-                  "oggetto": null,
-                  "nome": null}
-        selezionato["R"] = content
-        displayData()  
-      };
-
     });
   }
 }
 
 function readData() {
-  return fetch('file JSON/gruppi.json')
-    .then(response => response.json())
-    .then(result => {
-      data = result; 
+  return fetch("file JSON/gruppi.json")
+    .then((response) => response.json())
+    .then((result) => {
+      data = result;
     });
 }
 
-function displayData() {  
-  secondDiv.innerHTML = ''
-  Item = []
-  for (let key of Object.values(data[button])){
-    for (let value of key){
-      Item.push(value)
-    }
+function displayData() {
+  secondDiv.innerHTML = "";
+  Item = [];
+  for (let value of Object.values(data[button])) {
+    Item.push(value);
   }
-  
-  Item.forEach(item => {
-    secondDiv.insertAdjacentHTML('beforeend',`<li>${item}</li>`)
-  }) 
 
-  addNavListener('.nav.two > ul > li', 1)
+  Item.forEach((item) => {
+    secondDiv.insertAdjacentHTML("beforeend", `<li>${item}</li>`);
+  });
+
+  addNavListener(".nav.two > ul > li", 1);
 }
 
 readData().then(() => {
-  assignButtonIds('.nav.one > ul > li');
-  addNavListener('.nav.one > ul > li', 0);
+  assignButtonIds(".nav.one > ul > li");
+  addNavListener(".nav.one > ul > li", 0);
 
-  assignButtonIds('.nav.one > ul > li > i');
-  addNavListener('.nav.one > ul > li > i', 0);
+  assignButtonIds(".nav.one > ul > li > i");
+  addNavListener(".nav.one > ul > li > i", 0);
 });
-
-
