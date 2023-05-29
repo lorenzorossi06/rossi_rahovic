d2caffe6ffc62d42ab2bc16242f813b41f84c021
lorenzorossi06 <132877405+lorenzorossi06@users.noreply.github.com>
2023-05-29 08:40:46 +0200

    controllo input

--- SITO/script.js
+++ SITO/script.js
@@ -2,12 +2,15 @@ const secondDiv = document.querySelector(".nav.two > ul");
 let data;
 let button;
 let Item = [];
-let selezionato = { R: null, categoria: null, oggetto: null, nome: null };
+let selezionato = { "R": null, "categoria": null, "oggetto": null, "nome": null };
+
+const nav1 = document.querySelector('.nav.one > header');
+const nav2 = document.querySelector('.nav.two > header');
 
 const GRUPPI_APPARTENENZA = {
-  R1: ["frigoriferi", "congelatori", "condizionatori"],
+  R1: ["Frigoriferi", "Congelatori", "Condizionatori"],
 
-  R2: ["lavatrici", "lavastoviglie"],
+  R2: ["Lavatrici", "Lavastoviglie"],
 
   R3: ["TV Monitor a tubo catodico"],
 
@@ -15,14 +18,14 @@ const GRUPPI_APPARTENENZA = {
     "Elettronica di consumo",
     "Telecomunicazioni",
     "Informatica",
-    "piccoli elettrodomestici",
-    "elettroutensili",
-    "giocattoli",
-    "apparecchi di illuminazione",
-    "dispositivi medici",
+    "Piccoli elettrodomestici",
+    "Elettroutensili",
+    "Giocattoli",
+    "Apparecchi di illuminazione",
+    "Dispositivi medici",
   ],
 
-  R5: ["lampade fluorescenti", "sorgenti luminose compatte"],
+  R5: ["Lampade fluorescenti", "Sorgenti luminose compatte"],
 };
 
 function assignButtonIds(tag) {
@@ -45,6 +48,7 @@ function getR(object){
   for (let r of Object.keys(GRUPPI_APPARTENENZA)){
     for (let o of GRUPPI_APPARTENENZA[r]){
       if (object == o){
+        console.log(r)
         return r
       }
     }
@@ -65,11 +69,12 @@ function addNavListener(tag, funzione) {
         let r = getR(content)
         selezionato["oggetto"] = content;
         selezionato["R"] = r
-        // console.log(selezionato);
-
+        nav2.style.color = 'black';
+        
       } else {
         selezionato = {R: null, categoria: null, oggetto: null, nome: null};
         selezionato["categoria"] = content;
+        nav1.style.color = 'black';
         displayData();
       }
     });
@@ -99,6 +104,33 @@ function displayData() {
   addNavListener(".nav.two > ul > li", 1);
 }
 
+const submit = document.querySelector('button');
+const input = document.querySelector('input');
+
+submit.addEventListener('click', function() {
+  let nome = input.value;
+  nome = nome.trim()
+  if (nome == "" || nome == null){
+    submit.style.backgroundColor = "red"
+  } else{
+    submit.style.backgroundColor = "rgb(4, 255, 4)"
+  }
+
+  if( selezionato["categoria"] == null){
+    nav1.style.color = 'red';
+  }
+
+  if (selezionato["oggetto"] == null){
+    nav2.style.color = 'red';
+  }
+
+  else{
+    selezionato["nome"] = nome
+    console.log(selezionato)
+    selezionato = { "R": null, "categoria": null, "oggetto": null, "nome": null };
+  }
+});
+
 readData().then(() => {
   assignButtonIds(".nav.one > ul > li");
   addNavListener(".nav.one > ul > li", 0);
@@ -109,32 +141,34 @@ readData().then(() => {
 
 // gestione pulsanti
 
-const lista= document.querySelectorAll("li")
-const pulsante = document.querySelector("button")
-const nomeProd = document.querySelector(".desc").value
+// const lista= document.querySelectorAll("li")
+// const pulsante = document.querySelector("button")
+// const nomeProd = document.querySelector(".desc").value
 
-let elementiAttivi = []
+// let elementiAttivi = []
 
-function attivi(elems, attivi) {
-  attivi = []
-  console.log(attivi)
-  elems.forEach(elem => {
-    if (elem.classList.contains("active")) {
-      attivi.push(elem)
-    }
-  })
-  console.log(attivi)
-  if (attivi.length != 2) {
-    return false
-  } else {
-    return true
-  }
-}
-  document.addEventListener("click", () => {
-    let esito=attivi(lista, elementiAttivi)
-    console.log(esito)
-  })
+// function attivi(elems, attivi) {
+//   attivi = []
+//   console.log(attivi)
+//   elems.forEach(elem => {
+//     if (elem.classList.contains("active")) {
+//       attivi.push(elem)
+//     }
+//   })
+//   console.log(attivi)
+//   if (attivi.length != 2) {
+//     return false
+//   } else {
+//     return true
+//   }
+// }
+//   document.addEventListener("click", () => {
+//     let esito=attivi(lista, elementiAttivi)
+//     console.log(esito)
+//   })
 
-if (nomeProd != "") {
-  pulsante.style.backgroundColor = "green"
-}
+// if ( != "") {
+//   pulsante.style.backgroundColor = "green"
+// } else {
+//   pulsante.style.backgroundColor = "red"
+// }
